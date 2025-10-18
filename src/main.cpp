5e46ddafbeab50cdc7155421232b66428ab0b25e
microwha-ve <vilmer.ronqvist@proton.me>
2025-10-18 14:49:52 +0000

    Bot Init Made the actual bot thing, included more ignores into the gitignore bc i dont want to leak the token gaysss

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
+#include <dpp/dpp.h>
+#include <cstdlib>
 
+const std::string TOKEN = std::getenv(token);
 
 int main(){
-	std::cout << "Hello Hoes" << std::endl;
-	return 0;
+	dpp::cluster bot(TOKEN);
+
+	bot.on_log(dpp::utility::cout_logger());
+
+	bot.on_slashcommand([](const dpp::slashcommand_t& event){
+		if (event.command.get_command_name() == "ping"){
+			event.reply("Pong!");
+		}
+	});
+
+
+	bot.on_ready([&bot](const dpp::ready_t& event) {
+		if (dpp::run_once<struct register_bot_commands>()){
+			bot.global_command_create(dpp::slashcommand("ping", "Pong!", bot.me.id));
+		}
+	});
+
+	bot.start(dpp::st_wait);
+
 }
