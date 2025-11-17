c3f362b8f62b7490aa513ce0137bd4fc18834fe1
Microwh.ave <144789860+microwha-ve@users.noreply.github.com>
2025-11-17 19:41:06 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -174,7 +174,7 @@ int main() {
             }
 
             std::cout << userID << " has been banned with the reasoning: " << banReason << std::endl;
-            std::string msg = "<@" + userID.str() + "> has been banned for " + banReason + ".\n Banned by <@" + event.command.get_issuing_user().id.str + ">";
+            std::string msg = "<@" + userID.str() + "> has been banned for " + banReason + ".\n Banned by <@" + event.command.get_issuing_user().id.str() + ">";
             event.edit_response("The deed is done. The user has been removed.");
             bot.message_create(message(log_channel_id, msg));
           });
