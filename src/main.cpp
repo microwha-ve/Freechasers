5cbcd925b17c14316ce2160841d129c4b1acffb8
Microwh.ave <144789860+microwha-ve@users.noreply.github.com>
2025-11-17 19:24:51 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ int main() {
                         << "Gateway: **" << std::fixed << std::setprecision(2)
                         << gateway_ping_ms << " ms**\n"
                         << "REST: **" << std::fixed << std::setprecision(2)
-                        << rest_ping_ms << " ms**";
+                        << rest_ping_ms << " ms**\n" << "Development Branch";
 
                     // edit_original_response needs a dpp::message, not a string literal
                     event.edit_original_response(dpp::message(out.str()));
